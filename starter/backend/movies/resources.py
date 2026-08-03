b0d618072478fed150f938ca42b33dfda8fb25a0
blessedadudu <adududuke@gmail.com>
2026-08-03 16:23:59 +0100

    check error build -fix

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -11,9 +11,9 @@ movies = {
 
 class Movies(MethodView):
     def get(self, movie_id):
-        if movie_id is Noe:
+        if movie_id is None:
             # Return a list of all , yes!!!
-            return jsonify({movies": [dict({"title": movie["title"]}, **{"id": i}) for i, movie in movies.items()]})
+            return jsonify({"movies": [dict({"title": movie["title"]}, **{"id": i}) for i, movie in movies.items()]})
         else:
             # Return the details of a specific movie
             return jsonify({"movie": movies[str(movie_id)]})
